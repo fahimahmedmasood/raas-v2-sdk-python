2f94515c1cb5562780ec2db1806bcf7203338de3
Fahim Ahmed <fahimahmedmasood@gmail.com>
2018-08-14 16:31:23 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,8 @@ setup(
     url='https://apimatic.io/',
     packages=find_packages(),
     install_requires=[
-        'requests>=2.9.1, <3.0',
         'jsonpickle>=0.7.1, <1.0',
-        'cachecontrol>=0.11.7, <1.0',
-        'python-dateutil>=2.5.3, <3.0'
+        'cachecontrol>=0.11.7, <1.0'
     ],
     tests_require=[
         'nose>=1.3.7'
